0b8cff5a369abd5bb15094f3cdb8c2e35914aa80
BLVCK7 <101505525+BLVCK7@users.noreply.github.com>
2023-01-18 01:24:04 +0300

    Начал писать логику на изменение товара. Добавил в клиенте todo, где описал все, что нужно доделать

--- controlles/deviceController.js
+++ controlles/deviceController.js
@@ -2,6 +2,7 @@ const uuid = require('uuid');
 const path = require('path');
 const { Device, DeviceInfo, Brand, Type } = require('../models/models');
 const ApiError = require('../error/ApiError');
+const { Op } = require('sequelize');
 
 class DeviceController {
   async create(req, res, next) {
@@ -20,6 +21,69 @@ class DeviceController {
             highlights: i.highlights,
             deviceId: device.id,
           }),
+        );
+      }
+
+      return res.json(device);
+    } catch (error) {
+      next(ApiError.badRequest(error.message));
+    }
+  }
+
+  async getDeviceInfo(req, res, next) {
+    try {
+      let { name } = req.body;
+
+      const device = await Device.findAll({
+        where: {
+          name: { [Op.eq]: name },
+        },
+        include: [
+          {
+            model: DeviceInfo,
+            as: 'info',
+          },
+        ],
+      });
+
+      return res.json(device);
+    } catch (error) {
+      next(ApiError.badRequest(error.message));
+    }
+  }
+
+  async update(req, res, next) {
+    try {
+      let { name, price, brandId, typeId, info } = req.body;
+      const { img } = req.files;
+      let fileName = uuid.v4() + '.jpg';
+      img.mv(path.resolve(__dirname, '..', 'static', fileName));
+      const device = await Device.update(
+        { name, price, brandId, typeId, img: fileName },
+        {
+          where: {
+            name,
+            price,
+            brandId,
+            typeId,
+            img: fileName,
+          },
+        },
+      );
+
+      if (info) {
+        info = JSON.parse(info);
+        info.forEach((i) =>
+          DeviceInfo.update(
+            {
+              description: i.description,
+              highlights: i.highlights,
+              deviceId: device.id,
+            },
+            {
+              where: { description: i.description, highlights: i.highlights, deviceId: device.id },
+            },
+          ),
         );
       }
 
--- routes/deviceRouter.js
+++ routes/deviceRouter.js
@@ -3,8 +3,10 @@ const router = new Router();
 const DeviceController = require('../controlles/deviceController');
 const checkRoleMiddleware = require('../middleware/checkRoleMiddleware');
 
-router.post('/', checkRoleMiddleware('ADMIN'), DeviceController.create);
+router.post('/', DeviceController.create);
 router.get('/', DeviceController.getAll);
 router.get('/:id', DeviceController.getOne);
+router.patch('/', DeviceController.update);
+router.post('/info', DeviceController.getDeviceInfo);
 
 module.exports = router;
